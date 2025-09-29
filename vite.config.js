ac59c73f15df9b3f0ace7bc34a6a80ca5c450f27
joffchandler <68711006+joffchandler@users.noreply.github.com>
2025-09-29 13:02:22 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// If your repo name is different, change the string below to: '/<your-repo-name>/'
+export default defineConfig({
+  plugins: [react()],
+  base: '/sentinelsky-flight-ops/'
+})
