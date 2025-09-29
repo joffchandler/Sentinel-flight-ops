7bea9825057dc9f29ff4c162dd64a22f6222143c
joffchandler <68711006+joffchandler@users.noreply.github.com>
2025-09-29 16:00:25 +0100

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth.jsx';
+import UserCredentials from './components/UserCredentials.jsx';
 
 function TopNav() {
   return (
